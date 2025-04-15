a77011b823f2b39294e94c0401508f9c398abf30
dajer93 <varnai.martin@gmail.com>
2025-04-15 17:48:11 +0200

    fix type issue in polyfills

--- src/client/src/polyfills.ts
+++ src/client/src/polyfills.ts
@@ -1,9 +1,9 @@
 // Polyfill for 'global' used by amazon-cognito-identity-js
-if (typeof window !== 'undefined' && !window.global) {
+if (typeof window !== 'undefined' && !(window as any).global) {
   (window as any).global = window;
 }
 
 // Add other browser polyfills if needed
-if (typeof window !== 'undefined' && !window.process) {
+if (typeof window !== 'undefined' && !(window as any).process) {
   (window as any).process = { env: {} };
 } 
